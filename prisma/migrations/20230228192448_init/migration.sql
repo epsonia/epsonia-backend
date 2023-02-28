2cb2bcc0673c35966a9ce8e0e830fc7f06aaa1fc
maytees <mateespublicprofile@gmail.com>
2023-02-28 18:44:47 -0500

    rm

--- prisma/migrations/20230228192448_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Team" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "totalScore" INTEGER NOT NULL,
-    "windowsScore" INTEGER NOT NULL,
-    "linuxScore" INTEGER NOT NULL
-);
